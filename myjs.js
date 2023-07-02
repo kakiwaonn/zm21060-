ab6fe93c65c46fb7c7653c1deb38bcb9d221ae9d
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-07-02 15:48:31 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -7,7 +7,6 @@ alert('よろしくお願いします');
 
 <script>
   
-  function clickEvent() {alert('自主制作'); alert('アニメーション・画像です');
-}
+  function clickEvent() {alert('自主制作'); alert('アニメーション・画像です');}
 
   </script>
